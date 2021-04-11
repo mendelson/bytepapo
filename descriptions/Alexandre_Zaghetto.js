22ec37388aa2afaa43263f74bef3977243e81e2d
mendelson <m.mendelson.unb@gmail.com>
2021-04-11 20:27:56 -0300

    Change descriptions file structure

---
+++ descriptions/Alexandre_Zaghetto.js
@@ -0,0 +1,29 @@
+var id = 'Alexandre_Zaghetto';
+
+document.write(`
+    <p id="${id}" class="description mb-4 tm-intro-text text-justify">
+        Neste episódio, Alexandre Zaghetto conta sua história de vida desde sua infância até chegar na Sony do Vale do Silício. Já se perguntou como o tio da Kombi chegou até o Vale do Silício? Coloque seu fone de ouvido, pegue um lanchinho leve e venha nessa viagem!</br>
+        </br>
+        <iframe loading="lazy" src="https://open.spotify.com/embed-podcast/episode/1Bwn8iMBnNvOVmg7CKjqR0" width="100%" height="232" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+    </p>
+    
+    <hr id="line_1_${id}" class="description" />
+    <ul id="members_${id}" class="description">
+        <li>Apresentação: Mateus Mendelson</li>
+        <li>Convidado: Alexandre Zaghetto</li>
+        <li>Edição: Randi Maldonado</li>
+    </ul>
+
+    </br>
+    <hr id="line_2_${id}" class="description" /></br>
+
+    <p id="links_title_${id}" class="description">
+        Links úteis:</br>
+    </p>
+    <ul id="links_${id}" class="description">
+        <li><a class="regular-item" href="https://www.linkedin.com/in/zaghetto/" target="_blank">LinkedIn do Alexandre</a></li>
+        <li><a class="regular-item" href="http://alexandre.zaghetto.com/" target="_blank">Site pessoal do Alexandre</a></li>
+        <li><a class="regular-item" href="https://www.youtube.com/watch?v=PL6wD8jczkE" target="_blank">Que negócio é esse de Point Cloud?</a></li>
+        <li><a class="regular-item" href="https://www.saudebusiness.com/hospital/voc-sabe-como-surgiu-o-vale-do-silcio" target="_blank">Fonte das curiosidades</a></li>
+    </ul>
+    `);
---
+++ descriptions/Nasser_Boan.js
@@ -0,0 +1,26 @@
+var id = 'Nasser_Boan';
+
+document.write(`
+    <p id="${id}" class="description mb-4 tm-intro-text text-justify">
+        Neste episódio, Nasser Boan compartilha sua trajetória profissional, que é um tanto quanto improvável. Tendo uma formação acadêmica inicial totalmente desconexa com a ciência de dados, essa história é cheia de curiosidades. Coloque seu fone de ouvido, pegue um lanchinho leve e veja como uma brusca mudança de área pode alavancar uma carreira!</br>
+        </br>
+        <iframe loading="lazy" src="https://open.spotify.com/embed-podcast/episode/6xxQ3raQLhWvsO3Ocolh6x" width="100%" height="232" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+    </p>
+
+    <hr id="line_1_${id}" class="description" />
+    <ul id="members_${id}" class="description">
+        <li>Apresentação: Mateus Mendelson</li>
+        <li>Convidado: Nasser Boan</li>
+        <li>Edição: Randi Maldonado</li>
+    </ul>
+
+    </br>
+    <hr id="line_2_${id}" class="description" /></br>
+
+    <p id="links_title_${id}" class="description">
+        Links úteis:</br>
+    </p>
+    <ul id="links_${id}" class="description">
+        <li><a class="regular-item" href="https://www.linkedin.com/in/nasser-boan-2b264a85/" target="_blank">LinkedIn do Nasser</a></li>
+    </ul>
+    `);
