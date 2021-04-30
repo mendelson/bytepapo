5d8762e8423ea0df99f196e876a9e75266eda979
mendelson <m.mendelson.unb@gmail.com>
2021-04-29 22:31:45 -0300

    Add adsense

---
+++ descriptions/John_von_Neumann.js
@@ -0,0 +1,27 @@
+var id = 'John_von_Neumann';
+
+document.write(`
+    <p id="${id}" class="description mb-4 tm-intro-text text-justify">
+        Neste quadro de exatos 8 minutos, apresentamos a história de John von Neumann, matemático húngaro de origem judaica de grande destaque no mundo da computação. Separe um byte de minutos do seu dia e venha conosco no quadro Uma Byta História!</br>
+        </br>
+        <iframe loading="lazy" src="https://open.spotify.com/embed-podcast/episode/1Bwn8iMBnNvOVmg7CKjqR0" width="100%" height="232" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+    </p>
+    
+    <hr id="line_1_${id}" class="description" />
+    <ul id="members_${id}" class="description">
+        <li>Apresentação: Mateus Mendelson</li>
+        <li>Pesquisa biográfica: Hugo Tadashi</li>
+        <li>Edição: Randi Maldonado</li>
+    </ul>
+
+    </br>
+    <hr id="line_2_${id}" class="description" /></br>
+
+    <p id="links_title_${id}" class="description">
+        Links úteis:</br>
+    </p>
+    <ul id="links_${id}" class="description">
+        <li><a class="regular-item" href="https://www.linkedin.com/in/hugo-k-2b7476157/" target="_blank">LinkedIn do Hugo Tadashi</a></li>
+        <li><a class="regular-item" href="https://medium.com/trainingcenter/a-arquitetura-de-von-neumann-121489873fd4" target="_blank">A arquitetura de von Neumann</a></li>
+    </ul>
+    `);
