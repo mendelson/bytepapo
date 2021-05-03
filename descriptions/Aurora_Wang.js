e0f4dc44c827f23ee1b4b1ac261c39863cfe70da
mendelson <m.mendelson.unb@gmail.com>
2021-05-03 00:37:30 -0300

    Add S01.E03

---
+++ descriptions/Aurora_Wang.js
@@ -0,0 +1,31 @@
+var id = 'Aurora_Wang';
+
+document.write(`
+    <p id="${id}" class="description mb-4 tm-intro-text text-justify">
+        Neste episódio, Aurora Wang conta como foi recrutada para trabalhar em uma grande multinacional em Dublin. Aqui, sua história mostra a grande importância do engajamento na comunidade tecnológica e como isso pode criar novas oportunidades. Coloque seu fone de ouvido, pegue um lanchinho leve e venha nessa viagem!</br>
+        </br>
+        <iframe src="https://open.spotify.com/embed-podcast/episode/0Yn26Bs2wwy1a2KFIopUd6" width="100%" height="232" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+    </p>
+    
+    <hr id="line_1_${id}" class="description" />
+    <ul id="members_${id}" class="description">
+        <li>Apresentação: Mateus Mendelson</li>
+        <li>Convidado: Aurora Wang</li>
+        <li>Edição: Randi Maldonado</li>
+    </ul>
+
+    </br>
+    <hr id="line_2_${id}" class="description" /></br>
+
+    <p id="links_title_${id}" class="description">
+        Links úteis:</br>
+    </p>
+    <ul id="links_${id}" class="description">
+        <li><a class="regular-item" href="https://www.linkedin.com/in/auroralimin/" target="_blank">LinkedIn da Aurora</a></li>
+        <li><a class="regular-item" href="https://www.amazon.com.br/dp/0984782850/ref=cm_sw_em_r_mt_dp_9iH-Fb9WSZ89J" target="_blank">Recomendação de livro</a></li>
+        <li><a class="regular-item" href="https://github.com/brazil-it-groups/code-of-conduct#5-grupos-signatários-deste-guia" target="_blank">Links de comunidades</a></li>
+        <li><a class="regular-item" href="https://stackoverflow.com/jobs" target="_blank">Parte de empregos do Stack Overflow</a></li>
+        <li><a class="regular-item" href="https://www.amazon.jobs" target="_blank">Parte de empregos da Amazon</a></li>
+        <li><a class="regular-item" href="https://speakerdeck.com/auroralimin/dev-em-meio-ao-devops-mudanca-cultural-de-baixo-para-cima" target="_blank">Slides da palestra da Aurora sobre desenvolvimento profissional</a></li>
+    </ul>
+    `);
