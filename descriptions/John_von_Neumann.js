3a88a18aa05c203f6b2e6f24ee1c3e6e1d5e63ca
mendelson <m.mendelson.unb@gmail.com>
2021-05-17 00:58:41 -0300

    Add John von Neumann episode

--- descriptions/John_von_Neumann.js
+++ descriptions/John_von_Neumann.js
@@ -2,9 +2,11 @@ var id = 'John_von_Neumann';
 
 document.write(`
     <p id="${id}" class="description mb-4 tm-intro-text text-justify">
-        Neste quadro de exatos 8 minutos, apresentamos a história de John von Neumann, matemático húngaro de origem judaica de grande destaque no mundo da computação. Separe um byte de minutos do seu dia e venha conosco no quadro Uma Byta História!</br>
+        Neste episódio, apresentamos a história de John von Neumann, matemático húngaro de origem judaica de grande destaque no mundo da computação. Separe um byte de minutos do seu dia e venha conosco no quadro Uma Byta História!</br>
         </br>
-        <iframe loading="lazy" src="https://open.spotify.com/embed-podcast/episode/1Bwn8iMBnNvOVmg7CKjqR0" width="100%" height="232" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+        Confira o texto adaptado deste episódio <a class="regular-item" href="bytahistoria/${id}.html" onclick="gtag('event', 'Ver texto ep ${id}');">aqui</a>.</br>
+        </br>
+        <iframe loading="lazy" src="https://open.spotify.com/embed-podcast/episode/69iKJzkDgdgqbfdJFGZjw3" width="100%" height="232" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
     </p>
     
     <hr id="line_1_${id}" class="description" />
