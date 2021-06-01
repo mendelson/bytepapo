d496175e8b7711af22850115068b883be3a418b5
mendelson <m.mendelson.unb@gmail.com>
2021-06-01 10:39:31 -0300

    Add Arthur Pieri episode

---
+++ descriptions/Arthur_Pieri.js
@@ -0,0 +1,27 @@
+var id = 'Arthur_Pieri';
+
+document.write(`
+    <p id="${id}" class="description mb-4 tm-intro-text text-justify">
+        Neste episódio, Arthur Pieri conta sua experiência como empreendedor em Brasília e compartilha suas impressões sobre as falhas mais comuns entre jovens empreendedores. Coloque seu fone de ouvido, pegue um lanchinho leve e venha nessa viagem!</br>
+        </br>
+        <iframe src="https://open.spotify.com/embed/episode/3XGUNZ8jjKiibUMxDfA6pa" width="100%" height="232" frameBorder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+    </p>
+    
+    <hr id="line_1_${id}" class="description" />
+    <ul id="members_${id}" class="description">
+        <li>Apresentação: Mateus Mendelson</li>
+        <li>Convidado: Arthur Pieri</li>
+        <li>Edição: Randi Maldonado</li>
+    </ul>
+
+    </br>
+    <hr id="line_2_${id}" class="description" /></br>
+
+    <p id="links_title_${id}" class="description">
+        Links úteis:</br>
+    </p>
+    <ul id="links_${id}" class="description">
+        <li><a class="regular-item" href="https://www.linkedin.com/in/arthur-pieri/" target="_blank">LinkedIn do Arthur</a></li>
+        <li><a class="regular-item" href="https://arthurpieri.com/" target="_blank">Site pessoal do Arthur</a></li>
+    </ul>
+    `);
