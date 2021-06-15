9128812e74eab1a428026aa4d5c333aa073b7383
mendelson <m.mendelson.unb@gmail.com>
2021-06-15 11:06:35 -0300

    Fix missing Vitalik Buterin files

---
+++ descriptions/Vitalik_Buterin.js
@@ -0,0 +1,28 @@
+var id = 'Vitalik_Buterin';
+
+document.write(`
+    <p id="${id}" class="description mb-4 tm-intro-text text-justify">
+        Neste episódio, apresentamos a história de Vitalik Buterin, programador russo-canadense que foi um dos co-fundadores da plataforma Ethereum, cuja criptomoeda é a segunda maior em termos de capitalização de mercado. Separe um byte de minutos do seu dia e venha conosco no quadro Uma Byta História!</br>
+        </br>
+        Confira o texto adaptado deste episódio <a class="regular-item" href="bytahistoria/${id}.html" onclick="gtag('event', 'Ver texto ep ${id}');" target="_blank">aqui</a>.</br>
+        </br>
+        <iframe loading="lazy" src="https://open.spotify.com/embed/episode/6BsxTwZq8svHy7x2QZ307G" width="100%" height="232" frameBorder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+    </p>
+    
+    <hr id="line_1_${id}" class="description" />
+    <ul id="members_${id}" class="description">
+        <li>Apresentação: Mateus Mendelson</li>
+        <li>Pesquisa biográfica: Hugo Tadashi</li>
+        <li>Edição: Randi Maldonado</li>
+    </ul>
+
+    </br>
+    <hr id="line_2_${id}" class="description" /></br>
+
+    <p id="links_title_${id}" class="description">
+        Links úteis:</br>
+    </p>
+    <ul id="links_${id}" class="description">
+        <li><a class="regular-item" href="https://www.linkedin.com/in/hugo-k-2b7476157/" target="_blank">LinkedIn do Hugo Tadashi</a></li>
+    </ul>
+    `);
