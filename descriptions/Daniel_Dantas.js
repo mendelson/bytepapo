26bf209181e687f6cf54821a72b37e95b8f48b4a
mendelson <m.mendelson.unb@gmail.com>
2021-07-07 16:54:27 -0300

    Add Daniel Dantas episode

---
+++ descriptions/Daniel_Dantas.js
@@ -0,0 +1,30 @@
+var id = 'Daniel_Dantas';
+
+document.write(`
+    <p id="${id}" class="description mb-4 tm-intro-text text-justify">
+        Neste episódio, Daniel Dantas conta como um intercâmbio foi capaz de transformar sua vida, chegando a trabalhar como engenheiro de software na Microsoft mesmo tendo reprovado na universidade. Coloque seu fone de ouvido, pegue um lanchinho leve e venha nessa viagem!</br>
+        </br>
+        <iframe src="https://open.spotify.com/embed/episode/2ci5wUnQwsTcPxg0aotlFY" width="100%" height="232" frameBorder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+    </p>
+    
+    <hr id="line_1_${id}" class="description" />
+    <ul id="members_${id}" class="description">
+        <li>Apresentação: Mateus Mendelson</li>
+        <li>Convidado: Daniel Dantas</li>
+        <li>Edição: Randi Maldonado</li>
+    </ul>
+
+    </br>
+    <hr id="line_2_${id}" class="description" /></br>
+
+    <p id="links_title_${id}" class="description">
+        Links úteis:</br>
+    </p>
+    <ul id="links_${id}" class="description">
+        <li><a class="regular-item" href="https://www.linkedin.com/in/daniel-dantas-135684113/" target="_blank">LinkedIn do Daniel</a></li>
+        <li><a class="regular-item" href="https://youtu.be/9BZAMhT9yho" target="_blank">Dicas para entrevistas em grandes empresas</a></li>
+        <li><a class="regular-item" href="https://www.youtube.com/channel/UCt5mfpMmyx2WmYL23y9BltQ" target="_blank">Canal do Technagem no YouTube</a></li>
+        <li><a class="regular-item" href="https://www.amazon.com.br/Cracking-Coding-Interview-Programming-Questions/dp/0984782850/ref=sr_1_1?__mk_pt_BR=%C3%85M%C3%85%C5%BD%C3%95%C3%91&dchild=1&keywords=cracking+the+coding+interview&qid=1610841106&sr=8-1" target="_blank">Recomendação de Livro</a></li>
+        <li><a class="regular-item" href="https://leetcode.com/" target="_blank">LeetCode</a></li>
+    </ul>
+    `);
