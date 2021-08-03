37842853cbcb74ac0aaa8a1ddf0b27dc8c289e72
mendelson <m.mendelson.unb@gmail.com>
2021-08-03 02:32:40 -0300

    Add Cristiana de Farias episode

---
+++ descriptions/Cristiana_de_Farias.js
@@ -0,0 +1,33 @@
+var id = 'Cristiana_de_Farias';
+
+document.write(`
+    <p id="${id}" class="description mb-4 tm-intro-text text-justify">
+        Neste episódio, <a href="https://instagram.com/robo.cris" target="_blank">Cristiana de Farias</a> conta várias histórias curiosas sobre competições de futebol de robôs e ainda compartilha várias dicas de ouro sobre como estudar e se virar no exterior. Coloque seu fone de ouvido, pegue um lanchinho leve e venha nessa viagem!</br>
+        </br>
+        <iframe src="https://open.spotify.com/embed/episode/2kcbOtit4DfcYGVW0Kcqmi" width="100%" height="232" frameBorder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+    </p>
+    
+    <hr id="line_1_${id}" class="description" />
+    <ul id="members_${id}" class="description">
+        <li>Apresentação: Mateus Mendelson</li>
+        <li>Convidado: Cristiana de Farias</li>
+        <li>Edição: Randi Maldonado</li>
+    </ul>
+
+    </br>
+    <hr id="line_2_${id}" class="description" /></br>
+
+    <p id="links_title_${id}" class="description">
+        Links úteis:</br>
+    </p>
+    <ul id="links_${id}" class="description">
+        <li><a class="regular-item" href="https://www.linkedin.com/in/cristiana-miranda/" target="_blank">LinkedIn da Cristiana</a></li>
+        <li><a class="regular-item" href="https://cristianafarias.com/" target="_blank">Site pessoal da Cristiana</a></li>
+        <li><a class="regular-item" href="https://www.instagram.com/robo.cris/" target="_blank">Instagram da Cristiana sobre robótica</a></li>
+        <li><a class="regular-item" href="https://instagram.com/unbeatablesbr/" target="_blank">Instagram da UnBeatables</a></li>
+        <li><a class="regular-item" href="http://duerer.usc.edu/mailman/listinfo.cgi/robotics-worldwide" target="_blank">Uma lista de e-mails de robótica</a></li>
+        <li><a class="regular-item" href="https://www.eu-robotics.net/eurobotics/newsroom/mailing-list/index.html" target="_blank">Outra lista de e-mails de robótica</a></li>
+        <li><a class="regular-item" href="https://nccr-robotics.ch/students-academics/masters-internships-for-women/" target="_blank">Estágio para mulheres na Suíça em robótica</a></li>
+        <li><a class="regular-item" href="https://www.chevening.org" target="_blank">Programa Chevening</a></li>
+    </ul>
+    `);
