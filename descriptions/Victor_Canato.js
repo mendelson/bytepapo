e2aa91fb68655574466f8779174ad94f6ccb6b1e
mendelson <m.mendelson.unb@gmail.com>
2021-10-04 21:37:30 -0300

    Add Victor Canato episode

---
+++ descriptions/Victor_Canato.js
@@ -0,0 +1,30 @@
+var id = 'Victor_Canato';
+
+document.write(`
+    <p id="${id}" class="description mb-4 tm-intro-text text-justify">
+        Neste episódio, Victor Canato conta sobre sua caminhada profissional que, em um primeiro momento, parece tão aleatória quanto as aparições de Ronaldinho Gaúcho, mas que no fim mostram como ele se tornou um desenvolvedor Android sênior na Europa. Coloque seu fone de ouvido, pegue um lanchinho leve e venha nessa viagem!</br>
+        </br>
+        <iframe src="https://open.spotify.com/embed/episode/0Eb3T2eneY3qQIdtGQmMzi" width="100%" height="232" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
+    </p>
+    
+    <hr id="line_1_${id}" class="description" />
+    <ul id="members_${id}" class="description">
+        <li>Apresentação: Mateus Mendelson</li>
+        <li>Convidado: Victor Canato</li>
+        <li>Edição: Randi Maldonado</li>
+    </ul>
+
+    </br>
+    <hr id="line_2_${id}" class="description" /></br>
+
+    <p id="links_title_${id}" class="description">
+        Links úteis:</br>
+    </p>
+    <ul id="links_${id}" class="description">
+        <li><a class="regular-item" href="https://www.linkedin.com/in/vcanato/" target="_blank">LinkedIn do Canato</a></li>
+        <li><a class="regular-item" href="https://open.spotify.com/artist/0AXD8W8TIjwcZE746Pavo9?si=OnbWlfj1RCevVCaX_ZxwMQ&dl_branch=1" target="_blank">Músicas do Canato</a></li>
+        <li><a class="regular-item" href="https://nomadlist.com/" target="_blank">Mais sobre nomadismo digital</a></li>
+        <li><a class="regular-item" href="https://aiesec.org/" target="_blank">AIESEC</a></li>
+        <li><a class="regular-item" href="https://www.youtube.com/watch?v=y-5UKXnxCWg&t=6s&ab_channel=DevVailonge" target="_blank">Dev Vai Longe</a></li>
+    </ul>
+    `);
