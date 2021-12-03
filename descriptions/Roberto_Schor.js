92acafe3b1ac77a04785b7e914dd55234b0c0b31
mendelson <m.mendelson.unb@gmail.com>
2021-12-03 17:54:07 -0300

    Add Roberto Schor episode

---
+++ descriptions/Roberto_Schor.js
@@ -0,0 +1,32 @@
+var id = 'Roberto_Schor';
+
+document.write(`
+    <p id="${id}" class="description mb-4 tm-intro-text text-justify">
+        Neste episódio, Roberto Schor mostra a verdadeira definição de "ser raçudo"! Sabia que um atleta MVP também se destaca no mundo corporativo? Coloque seu fone de ouvido, pegue um lanchinho leve e venha nessa viagem!</br>
+        </br>
+        <iframe src="https://open.spotify.com/embed/episode/0Eb3T2eneY3qQIdtGQmMzi" width="100%" height="232" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
+    </p>
+    
+    <hr id="line_1_${id}" class="description" />
+    <ul id="members_${id}" class="description">
+        <li>Apresentação: Mateus Mendelson</li>
+        <li>Convidado: Roberto Schor</li>
+        <li>Edição: Randi Maldonado</li>
+    </ul>
+
+    </br>
+    <hr id="line_2_${id}" class="description" /></br>
+
+    <p id="links_title_${id}" class="description">
+        Links úteis:</br>
+    </p>
+    <ul id="links_${id}" class="description">
+        <li><a class="regular-item" href="https://www.linkedin.com/in/robertorschor/" target="_blank">LinkedIn do Roberto Schor</a></li>
+        <li><a class="regular-item" href="https://www.facebook.com/watch/?v=1057241407663926" target="_blank">Beto chamando pra jogo!</a></li>
+        <li><a class="regular-item" href="https://www.youtube.com/watch?v=73a3fJ7F83s&ab_channel=RobertoSchor target="_blank">Vídeo do porquê não arrumar problemas com o Beto</a></li>
+        <li><a class="regular-item" href="https://www.youtube.com/watch?v=eP0FYeO1N2A" target="_blank">Vídeo sobre jogadores profissionais</a></li>
+        <li><a class="regular-item" href="https://www.mg.superesportes.com.br/app/noticias/mais-esportes/2013/03/08/noticia_maisesportes,244386/belo-horizonte-recebe-seletiva-para-selecao-brasileira-de-futebol-americano.shtml" target="_blank">Seletiva da seleção brasileira em Minas Gerais</a></li>
+        <li><a class="regular-item" href="https://openfoam.org/" target="_blank">Software OpenFOAM</a></li>
+        <li><a class="regular-item" href="https://databootcamp.com.br/" target="_blank">Data Bootcamp</a></li>
+    </ul>
+    `);
