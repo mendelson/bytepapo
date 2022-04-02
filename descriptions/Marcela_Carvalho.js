8cdc3ca05774ded504ee2ad039c8e4524c8f7655
mendelson <m.mendelson.unb@gmail.com>
2022-04-01 22:39:49 -0300

    Add Marcela Carvalho episode files

---
+++ descriptions/Marcela_Carvalho.js
@@ -0,0 +1,27 @@
+var id = 'Marcela_Carvalho';
+
+document.write(`
+    <p id="${id}" class="description mb-4 tm-intro-text text-justify">
+        Neste episódio, Marcela Carvalho conta como foi acabar trabalhando em Paris com pesquisa e desenvolvimento para smart cities. Coloque seu fone de ouvido, pegue um lanchinho leve e venha nessa viagem!</br>
+        </br>
+        <iframe style="border-radius:12px" src="https://open.spotify.com/embed/episode/2Ag7SP5CKakXh9M9sy1lMK?utm_source=generator" width="100%" height="232" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
+    </p>
+    
+    <hr id="line_1_${id}" class="description" />
+    <ul id="members_${id}" class="description">
+        <li>Apresentação: Mateus Mendelson</li>
+        <li>Convidado: Marcela Carvalho</li>
+        <li>Edição: Randi Maldonado</li>
+    </ul>
+
+    </br>
+    <hr id="line_2_${id}" class="description" /></br>
+
+    <p id="links_title_${id}" class="description">
+        Links úteis:</br>
+    </p>
+    <ul id="links_${id}" class="description">
+        <li><a class="regular-item" href="https://www.linkedin.com/in/marcela-pcarvalho/" target="_blank">LinkedIn da Marcela Carvalho</a></li>
+        <li><a class="regular-item" href="https://www.upciti.com/en/" target="_blank">Conheça a Upciti</a></li>
+    </ul>
+    `);
