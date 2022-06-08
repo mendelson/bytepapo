2696cdd2df1b0164b7a8948e07cfd2871056c3b8
mendelson <m.mendelson.unb@gmail.com>
2022-06-07 22:40:11 -0300

    Add Felipe Begni episode files

---
+++ descriptions/Felipe_Begni.js
@@ -0,0 +1,28 @@
+var id = 'Felipe_Begni';
+
+document.write(`
+    <p id="${id}" class="description mb-4 tm-intro-text text-justify">
+        Neste episódio, Felipe Begni conta um pouco a respeito da carreira de Product Manager e também sobre como foi seu percurso ao entrar como estagiário e crescer até se tornar gerente na OLX Brasil. Nesse episódio, nosso convidado deu um show. Literalmente! Coloque seu fone de ouvido, pegue um lanchinho leve e venha nessa viagem!</br>
+        </br>
+        <iframe style="border-radius:12px" src="https://open.spotify.com/embed/episode/43KMIr75EqLi6tSOXLzBT3?utm_source=generator" width="100%" height="232" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
+    </p>
+    
+    <hr id="line_1_${id}" class="description" />
+    <ul id="members_${id}" class="description">
+        <li>Apresentação: Mateus Mendelson</li>
+        <li>Convidado: Felipe Begni</li>
+        <li>Edição: Randi Maldonado</li>
+    </ul>
+
+    </br>
+    <hr id="line_2_${id}" class="description" /></br>
+
+    <p id="links_title_${id}" class="description">
+        Links úteis:</br>
+    </p>
+    <ul id="links_${id}" class="description">
+        <li><a class="regular-item" href="https://www.linkedin.com/in/felipe-begni-padilha-110a04149/" target="_blank">LinkedIn do Felipe Begni</a></li>
+        <li><a class="regular-item" href="https://www.netflix.com/br/title/81157840" target="_blank">Dieta de Gladiadores, na Netflix</a></li>
+        <li><a class="regular-item" href="https://careers.smartrecruiters.com/olxbrasil" target="_blank">Vagas de emprego na OLX Brasil</a></li>
+    </ul>
+    `);
