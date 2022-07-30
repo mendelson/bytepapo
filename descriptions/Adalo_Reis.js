006570d771fd8b40b9e622eb4f8ad0fdf19d37dc
mendelson <m.mendelson.unb@gmail.com>
2022-07-30 01:06:01 -0300

    Add Adalo Reis episode files

---
+++ descriptions/Adalo_Reis.js
@@ -0,0 +1,26 @@
+var id = 'Adalo_Reis';
+
+document.write(`
+    <p id="${id}" class="description mb-4 tm-intro-text text-justify">
+        Neste episódio, Ádalo Reis conta um pouco da sua história desde o interior de Minas até entrar no mundo de vendas de software e hardware. E claro que também mostra sua paixão pela Harley-Davidson. Coloque seu fone de ouvido, pegue um lanchinho leve e venha nessa viagem!</br>
+        </br>
+        <iframe style="border-radius:12px" src="https://open.spotify.com/embed/episode/43KMIr75EqLi6tSOXLzBT3?utm_source=generator" width="100%" height="232" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
+    </p>
+    
+    <hr id="line_1_${id}" class="description" />
+    <ul id="members_${id}" class="description">
+        <li>Apresentação: Mateus Mendelson</li>
+        <li>Convidado: Ádalo Reis</li>
+        <li>Edição: Randi Maldonado</li>
+    </ul>
+
+    </br>
+    <hr id="line_2_${id}" class="description" /></br>
+
+    <p id="links_title_${id}" class="description">
+        Links úteis:</br>
+    </p>
+    <ul id="links_${id}" class="description">
+        <li><a class="regular-item" href="https://www.linkedin.com/in/adaloreis/" target="_blank">LinkedIn do Ádalo Reis</a></li>
+    </ul>
+    `);
