9adb06cfddb18f7cad237519b77da51307c3c398
mendelson <m.mendelson.unb@gmail.com>
2023-03-20 13:55:32 -0300

    Add Fernando Siqueira episode files

---
+++ descriptions/Fernando_Siqueira.js
@@ -0,0 +1,26 @@
+var id = 'Fernando_Siqueira';
+
+document.write(`
+    <p id="${id}" class="description mb-4 tm-intro-text text-justify">
+        Neste episódio, Fernando Siqueira conta sua jornada até se tornar gestor em ciência de dados no Nubank, faz um dueto sensacional e, ainda por cima, compartilha algumas histórias de amor! Coloque seu fone de ouvido, pegue um lanchinho leve e venha nessa viagem!</br>
+        </br>
+        <iframe style="border-radius:12px" src="https://open.spotify.com/embed/episode/57Ht743z8BcMun7jABXVwU?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+    </p>
+    
+    <hr id="line_1_${id}" class="description" />
+    <ul id="members_${id}" class="description">
+        <li>Apresentação: Mateus Mendelson</li>
+        <li>Convidado: Fernando Siqueira</li>
+        <li>Edição: Randi Maldonado</li>
+    </ul>
+
+    </br>
+    <hr id="line_2_${id}" class="description" /></br>
+
+    <p id="links_title_${id}" class="description">
+        Links úteis:</br>
+    </p>
+    <ul id="links_${id}" class="description">
+        <li><a class="regular-item" href="https://www.linkedin.com/in/fernando-de-lucca-siqueira-6479b72a/" target="_blank">LinkedIn do Fernando Siqueira</a></li>
+    </ul>
+    `);
